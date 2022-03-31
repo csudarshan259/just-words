0ada744a5516803efd665e266fcde49875db2416
Sudarshan Chavan <csudarshan259@gmail.com>
2022-03-31 14:00:48 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WordsComponent } from './words/words.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:WordsComponent},
+  {path:'**',redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
